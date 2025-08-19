4acb4f0f03c68887071acc8682b1e23aa8dd1259
Ilyas <SultanovIlyas0.com>
2025-08-19 19:20:55 +0500

    fead: Configuration for db and auth

---
+++ src/core/config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent.parent
+
+class Auth(BaseModel):
+    private_key_path: Path = BASE_DIR / "cearts" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "cearts" / "jwt-public.pem"
+    algorithm: str = "RS256"
+
+
+class Settings(BaseSettings):
+
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
+
+    db_url: str = "sqlite+aiosqlite:///db.sqlite3"
+
+    auth_jwt: Auth = Auth()
+
+
+settings = Settings()
