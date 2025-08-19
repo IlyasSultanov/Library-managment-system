6404b3a34fd605affd70ab9f36b0d91277da5923
Ilyas <SultanovIlyas0.com>
2025-08-19 20:06:18 +0500

    fix: Fix settings url db

--- src/core/config.py
+++ src/core/config.py
@@ -1,8 +1,15 @@
+"""
+Config module for the application.
+
+This module provides the configuration for the application.
+"""
+import os
 from pathlib import Path
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).parent.parent
+
 
 class Auth(BaseModel):
     private_key_path: Path = BASE_DIR / "cearts" / "jwt-private.pem"
@@ -14,7 +21,7 @@ class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
 
-    db_url: str = "sqlite+aiosqlite:///db.sqlite3"
+    db_url: str = os.getenv("URL")
 
     auth_jwt: Auth = Auth()
 
